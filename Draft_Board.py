3e6049ce15538b30620274c44d65c9f4ab95989f
Jianxuan Xiong <jianxuan.xiong@polimi.it>
2025-01-29 18:09:17 +0100

    Testing the wavefront reconstruction with Zernikes

--- Draft_Board.py
+++ Draft_Board.py
@@ -6,10 +6,9 @@ os.chdir(current_directory)
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-from Misc_Tools import frankot_chellappa, peaks, wavefront_gradient, wavefront_gradient_test
+from Misc_Tools import frankot_chellappa, peaks, wavefront_reconstruction, show_wavefront
 from Zernike_Polynomials_Modules import show_profile, min_circle, image_padding_for_circular_area
 from Coordinates_Finder_Modules import precise_coord, show_coord
-from scipy import signal
 
 ran = 0.25
 voltage_to_coord = np.load(f"Data_Deposit/range_{ran}_probe_coordinates_diff.npy") / ran
@@ -23,20 +22,11 @@ padded_ref_coord = precise_coord(padded_ref_img)
 padded_ref_circle = min_circle(padded_ref_coord, scale = 1.2)
 
 zern_coff = np.zeros((27, 1))
-zern_coff[7] = 1
+zern_coff[3] = 1
 zern_volt = np.einsum("ij, ik -> jk", zern_coff, zern_to_voltage).T
 zern_disp = np.einsum("ij, ik -> jk", zern_volt, voltage_to_coord).reshape(-1, 2)
-zern_gradient_x, zern_gradient_y, zern_grid_x, zern_grid_y = wavefront_gradient_test(padded_ref_coord, padded_ref_coord + zern_disp)
 
-peaks_z, peaks_grid_x, peaks_grid_y = peaks(160, 160)
-peaks_gradient_x, peaks_gradient_y = np.gradient(peaks_z)
+zern_reconstruct, zern_grid_x, zern_grid_y = wavefront_reconstruction(padded_ref_coord, padded_ref_coord + zern_disp)
+show_wavefront(zern_reconstruct, zern_grid_x, zern_grid_y)
 
-re_zern = frankot_chellappa(zern_gradient_x, zern_gradient_y)
-# re_zern = frankot_chellappa(-zern_gradient_x, zern_gradient_y)
-# re_peaks = frankot_chellappa(peaks_gradient_x, peaks_gradient_y)
-# show_profile(re_peaks, peaks_grid_x, peaks_grid_y)
-# show_profile(peaks_z, peaks_grid_x, peaks_grid_y)
-show_profile(re_zern, zern_grid_x, zern_grid_y)
-# show_coord(padded_ref_img, padded_ref_coord + zern_disp)
-print(f"P-V: {np.max(re_zern) - np.min(re_zern)}")
-plt.show()
+print(f"P-V: {np.max(zern_reconstruct) - np.min(zern_reconstruct)}")
