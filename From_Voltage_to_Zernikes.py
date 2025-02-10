9d3c96f2b708a218e33bdb87165168b111b8fb0a
Jianxuan Xiong <jianxuan.xiong@polimi.it>
2025-02-10 17:33:22 +0100

    Field test & recalculation

--- From_Voltage_to_Zernikes.py
+++ From_Voltage_to_Zernikes.py
@@ -68,12 +68,12 @@ if __name__ == "__main__":
     voltage_to_coord = np.load(f"Data_Deposit/range_{ran}_probe_coordinates_diff.npy")/ran
     coord_to_voltage = np.linalg.pinv(voltage_to_coord)
     reference_coord = np.load(f'Data_Deposit/range_{ran}_reference_coordinates.npy')
-    reference_circle = min_circle(reference_coord, scale = 1.25)
+    reference_circle = min_circle(reference_coord, scale = 1.1)
     # captured_frames = np.load(f'Data_Deposit/range_{ran}_probe_captured_frames.npy')
     reference_frame = np.load(f'Data_Deposit/range_{ran}_probe_reference_image.npy')
     padded_ref_img = image_padding_for_circular_area(reference_frame, reference_circle, cut = True)
     padded_ref_coord = precise_coord(padded_ref_img)
-    padded_ref_circle = min_circle(padded_ref_coord, scale = 1.13)
+    padded_ref_circle = min_circle(padded_ref_coord, scale = 1.1)
     # show_coord(padded_ref_img, padded_ref_coord, padded_ref_circle)
     # plt.show(), input()
 
